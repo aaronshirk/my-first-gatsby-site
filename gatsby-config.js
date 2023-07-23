53c8da62e09ee0125fb63f638843c3583c0ef894
Aaron Shirk <aaronshirk@gmail.com>
2023-07-23 08:57:27 -0400

    data in building block components

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   siteMetadata: {
     title: `My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
+    description: "Description of my site.",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
